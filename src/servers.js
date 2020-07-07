b7a05f9c15c6d8d3e755c133f273c1d208843a5d
MechSlayer <iuliranetescu@gmail.com>
2020-07-07 22:58:00 +0200

    Arreglado RegEx al obtener los servidores

--- src/servers.js
+++ src/servers.js
@@ -8,8 +8,8 @@ async function getServer(themeCode)
     let theme = split[1];
     /**@type {string} */
     let page = await requests.get(`https://${lang}.akinator.com`);
-    let regex = /{\"translated_theme_name\":\"[\s\S]*\",\"urlWs\":\"https:\\\/\\\/srv[0-9]+\.akinator\.com:[0-9]+\\\/ws\",\"subject_id\":\"[0-9]+\"}/gim
-    let parsed = page.match(regex).map(e => JSON.parse(e));
+    let regex = /\[{\"translated_theme_name\":\"[\s\S]*\",\"urlWs\":\"https:\\\/\\\/srv[0-9]+\.akinator\.com:[0-9]+\\\/ws\",\"subject_id\":\"[0-9]+\"}]/gim
+    let parsed = JSON.parse(page.match(regex));
     let obj = {};
     for (let sv of parsed) obj[sv.translated_theme_name.toLowerCase()] = sv;
     if (!theme) return Object.values(obj)[0];
